9fdaf917fd3f9119024aeb9c2c1467c7ce26c462
abhaysaqi <abhaysaqi9@gmail.com>
2025-10-16 14:42:39 +0530

    fix import issues

--- mytestapp/lib/app/features/home/repository/home_repository.dart
+++
@@ -1,9 +0,0 @@
-import '../model/home_model.dart';
-
-abstract class HomeRepository {
-  Future<List<HomeModel>> getHomes();
-  Future<HomeModel> getHomeById(String id);
-  Future<HomeModel> createHome(HomeModel home);
-  Future<HomeModel> updateHome(HomeModel home);
-  Future<void> deleteHome(String id);
-}
