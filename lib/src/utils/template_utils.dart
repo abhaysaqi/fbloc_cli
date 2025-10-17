df7232cf9e5d4cd677beb7ecca72a2b18b74d3ba
abhaysaqi <abhaysaqi9@gmail.com>
2025-10-17 14:38:50 +0530

    import repository and service in main

--- lib/src/utils/template_utils.dart
+++ lib/src/utils/template_utils.dart
@@ -56,6 +56,8 @@ import 'app/routes/app_routes.dart';
 import 'app/features/home/$stateFolder/home_${config.stateManagement}.dart';
 import 'app/features/home/repository/home_repository.dart';
 import 'app/features/home/repository/home_repository_impl.dart';
+import 'app/core/service/api_service.dart';
+import 'app/features/home/bloc/home_event.dart';
 
 void main() {
   runApp(const MyApp());
@@ -97,6 +99,8 @@ import 'app/routes/route_names.dart';
 import 'app/features/home/$stateFolder/home_${config.stateManagement}.dart';
 import 'app/features/home/repository/home_repository.dart';
 import 'app/features/home/repository/home_repository_impl.dart';
+import 'app/core/service/api_service.dart';
+import 'app/features/home/bloc/home_event.dart';
 
 void main() {
   runApp(const MyApp());
